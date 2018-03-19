4f4ad339663c7c5c5cc79d238dba73fa018d7607
Sung <ysung88@gmail.com>
2018-03-19 17:57:52 -0400

    Base implementation from class diagram.  Changed Product to public.

--- PHFoodManagement/Order.cs
+++ PHFoodManagement/Order.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace PHFoodManagement
 {
-    class Order
+    public class Order
     {
+        private int _orderNumber;
+        //private Client _client;
+        private bool _paid;
+        //private List<OrderItem> _orderItems;
+        private DateTime _deliveryDate;
+        private DateTime _orderDate;
+
+        //TODO need OrderItem and Client classes to have the constructor created
+        //public Order (Client client, OrderItem item)
+        //{
+            
+        //}
+
+        public decimal CalculateTotal()
+        {
+            //iterate through all orderitems and calculate the total of this order
+            //sum of product quantity * (product cost - client.extraDiscount)
+            throw new NotImplementedException("Not yet implemented");
+        }
+
+        public void AddProduct(Product product, double quantity)
+        {
+            //add the product with quantity to orderitem list
+            //_orderItems.Add(new OrderItem(product, quantity));
+        }
     }
 }
--- PHFoodManagement/Product.cs
+++ PHFoodManagement/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PHFoodManagement
 {
-    class Product
+    public class Product
     {
     }
 }
